16852d217ef4962868dd66debc110ef644ea8d3c
Raithenx <32513277+Raithenx@users.noreply.github.com>
2017-11-15 12:50:00 +0100

    Początek strony

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once('config/errors.php');
+ob_start();
+require_once('config/database.php');
+session_start();
+
+if(empty($_GET['a'])) $_GET['a'] = "home";
+$action = $_GET['a'];
+
+$home = array(
+"home",
+);
+
+$page = array(
+"start",
+"logout",
+"pracownicy",
+"stanowiska",
+"dodaj",
+);
+
+if(in_array($action, $page)){
+	require_once('page/index.php');
+}
+elseif(in_array($action, $home)){
+	require_once('home/index.php');
+}
+else{
+	require_once('error.php');
+}
+
+ob_end_flush();
+?>
