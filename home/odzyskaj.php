a6516621ebf2a743102f82da23c45f93fe1936f9
lhoro <32704441+lhoro@users.noreply.github.com>
2018-01-09 22:11:02 +0100

    Add files via upload

---
+++ home/odzyskaj.php
@@ -0,0 +1,74 @@
+<?php
+echo "<CENTER>Odzyskiwanie hasła
+<form action='?a=odzyskaj' method='post'>
+	<table>
+		<tr>
+			<td>
+			Login:
+			</td>
+			<td>
+			<input type='text' style='width:200px' name='login' />
+			<td>
+		</tr>
+		<tr>
+			<td>
+				E-mail:
+			</td>
+			<td>
+				<input type='text' style='width:200px' name='email' />
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+			</td>
+			<td>
+				<input type='submit' style='width:100px' value='Odzyskaj'/>
+			</td>
+		</tr>
+	</table>
+</form>
+</CENTER>";
+
+if(!empty($_POST)){
+	if (!empty($_POST['login']) && (!empty($_POST['email']))){
+		$login=$_POST['login'];
+		$email=$_POST['email']; 
+		
+		//sprawdzenie poprawności
+		$dane = mysqli_query($db , "SELECT * FROM WebSiteUsers WHERE Login='$login' LIMIT 1");
+		if (mysqli_num_rows($dane)==1){
+			$dane=mysqli_fetch_assoc($dane);
+			if ($email==$dane['Email']){
+				
+				function losowy_ciag($dlugosc){
+				   $string = md5(time());
+				   $string = substr($string,0,$dlugosc);
+				   return($string);
+				}
+				$nowehaslo = losowy_ciag(10);
+
+				$to = $email;
+				$subject = "Odzyskiwanie konta do strony zakladowej";
+				$message = "Nowe hasło do strony zakładowej: ".$nowehaslo;
+				$from = "konto.zakladu@gmail.pl";
+				$headers = "From:".$from;
+				mail($to,$subject,$message,$headers);
+				$md5haslo = md5($nowehaslo);
+				mysqli_query($db ,"UPDATE `WebSiteUsers` SET Haslo = '".$md5haslo."' WHERE Login='".$login."';");
+				echo "<CENTER>Wysłano nowe hasło na podany adres email.</BR>
+					<a href='?a=home'>Powrót</a></CENTER>";
+			}
+			else {
+				echo '<CENTER>Podane email jest niepoprawny!</CENTER><BR>';
+			}
+		}
+		else {
+			echo '<CENTER>Nie znaleziono takiego loginu w bazie.</CENTER><BR>';
+		}		
+	}
+	else{
+		echo '<CENTER>Podaj swój login oraz adres email.</CENTER><BR>';
+	}
+};
+?>
