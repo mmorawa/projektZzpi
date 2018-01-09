e8b2d2109d90dd3a424acf58ff8fc7e1ab392044
lhoro <32704441+lhoro@users.noreply.github.com>
2018-01-09 22:11:30 +0100

    Add files via upload

--- home/home.php
+++ home/home.php
@@ -27,19 +27,32 @@ echo "<CENTER>Logowanie
 			</td>
 		</tr>
 	</table>
-</form></CENTER>";
+</form>
+	<table>
+		<tr>
+			<td> Nie pamiętasz hasła? 
+			</td>
+			<td>
+				<a href='?a=odzyskaj'>Odzyskaj hasło</a>
+			</td>
+		</tr>
+	</table>
+</CENTER>";
 
 if(!empty($_POST)){
 	if (!empty($_POST['login']) && (!empty($_POST['password']))){
 		$login=$_POST['login'];
-		$password=$_POST['password'];
+		$password=$_POST['password']; 
 		//sprawdzenie poprawności
-		$dane = mysqli_query($db , "SELECT * FROM websiteusers WHERE Login='$login' LIMIT 1");
+		$dane = mysqli_query($db , "SELECT * FROM WebSiteUsers WHERE Login='$login' LIMIT 1");
 		if (mysqli_num_rows($dane)===1){
 			$dane=mysqli_fetch_assoc($dane);
-			if (md5($password)===$dane['Haslo']){
+			if (md5( $password)===$dane['Haslo']){
                 //logowanie i pobranie sesji
-				$_SESSION['id'] = $dane['Id'];
+				$_SESSION['id'] = $dane['Id']; 
+				$dane = mysqli_query($db , "SELECT IdStanowiska FROM Pracownicy WHERE Id=".$_SESSION['id']." LIMIT 1");
+				$dane = mysqli_fetch_array($dane);
+				$_SESSION['stanowisko'] = $dane['0'];
 				header("Location: ?a=start");
 			}
 			else {
